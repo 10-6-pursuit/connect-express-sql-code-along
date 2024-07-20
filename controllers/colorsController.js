31676fd5461edb103405391a6090ee2dda44a8f4
RazEfron <razefron01@gmail.com>
2024-07-20 09:33:53 -0400

    start of delete and update lesson

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -2,16 +2,18 @@ const express = require("express");
 const colors = express.Router();
 const { getAllColors, getColor, createColor } = require("../queries/color");
 const { checkName, checkBoolean } = require("../validations/checkColors");
+
 // Index
 colors.get("/", async (req, res) => {
   const allColors = await getAllColors();
   if (allColors[0]) {
     res.status(200).json(allColors);
   } else {
-    res.status(500).json({ error: "server error" });
+    res.status(500).json({ error: allColors });
   }
 });
 
+// Show
 colors.get("/:id", async (req, res) => {
   const { id } = req.params;
   const color = await getColor(id);
@@ -22,6 +24,7 @@ colors.get("/:id", async (req, res) => {
   }
 });
 
+// Create
 colors.post("/", checkName, checkBoolean, async (req, res) => {
   const color = await createColor(req.body);
   res.json(color);
