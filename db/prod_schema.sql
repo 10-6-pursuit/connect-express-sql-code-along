5d12f05667b3b209274b6ef85789fcfb4fe6dada
RazEfron <razefron01@gmail.com>
2024-07-21 12:42:18 -0400

    change prod db to the correct name

--- db/prod_schema.sql
+++ db/prod_schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS colors_prod;
-CREATE DATABASE colors_prod;
+DROP DATABASE IF EXISTS colors_prod_4r4u;
+CREATE DATABASE colors_prod_4r4u;
 
-\c colors_prod;
+\c colors_prod_4r4u;
 
 CREATE TABLE colors (
     id SERIAL PRIMARY KEY,
--- db/prod_seed.sql
+++ db/prod_seed.sql
@@ -1,4 +1,4 @@
-\c colors_prod;
+\c colors_prod_4r4u;
 
 INSERT INTO
     colors (name, is_favorite)
